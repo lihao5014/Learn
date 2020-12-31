bec5217b2ab02a0609fcc68c303dd13b2a36fb8b
18200399814 <2272203582@qq.com>
2020-12-31 23:12:16 +0800

    1.学习函数模板特例化和两种具体化（显示具体化、隐具体化）的使用方法。 2.研究函数返回引用的使用情况（函数可以将传入参数和静态局部变量的引用返回，但不能将函数中的普通局部变量的引用返回）。

--- C++/Language/reference/sample.cpp
+++ C++/Language/reference/sample.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+#define _ERROR_
+#undef _ERROR_
+
+#define _CHANGE_WAY_
+#undef _CHANGE_WAY_
+
 using namespace std;
 
 class Sample
 {
 public:
-	Sample(int n){this->n = n;}
+	Sample(int n){cout<<"constructor"<<endl;this->n = n;}
+	Sample(const Sample& other):n(other.n){cout<<"copy constructor"<<endl;}
 	~Sample(){cout<<"destruction"<<endl;}
 	
 	void setNum(const int n){this->n = n;}
@@ -15,16 +22,25 @@ private:
 	int n;
 };
 
-int& fun(int &n)
+int& fun(int &n)    //函数返回引用时，不会调用拷贝构造，或做内存拷贝操作。
 {
-#if 1
-	static int m;   //函数不能反回局部变量的引用
+#ifdef _ERROR_
+	int m;           //函数不能返回函数中创建的普通局部变量引用
 	m = n*n;
+	cout<<"can't return normal local variable reference"<<endl;
+	return m;
+#else     //_ERROR_
+#ifdef _CHANGE_WAY_
+	static int m;     //函数可以返回函数中创建的静态局部变量引用
+	m = n*n;
+	cout<<"return static local variable reference"<<endl;
 	return m;
 #else
 	n = n*n;
+	cout<<"return parameter variable reference"<<endl;
 	return n;
-#endif
+#endif    //_CHANGE_WAY_
+#endif    //_ERROR_
 }
 
 Sample& add(Sample& s1,Sample s2)
@@ -35,21 +51,44 @@ Sample& add(Sample& s1,Sample s2)
 
 int main()
 {
-	Sample num(5);
-	int n = 2;
+	int x = 2;
+	int y = fun(x);          //因为创建新变量，所以赋值运算符会将fun()函数返回的引用值，拷贝给新创建的变量。
+	cout<<"x ="<<x<<" ,y ="<<y<<endl;
 	
+	x += 1;
+	int& z = fun(x);        //因为创建新引用，所以新的引用会绑定fun()函数返回的引用，赋值运算符不做内存拷贝操作。
+	cout<<"x ="<<x<<" ,z ="<<z<<endl;
+	
+	x++;
+	fun(x);
+	cout<<"x ="<<x<<" ,z ="<<z<<endl;
+	
+	double &ref = *(new double(2.71828));    //C++中的使引用直接指向堆变量的用法，就类似于Java或C#中的引用概念。
+	cout<<"ref ="<<ref<<endl;                //只是C++中的引用是强绑定，一旦引用初始化时绑定了一个变量名，就不能再修改其绑定到其他变量上。
+	                                         //而Java或C#中的引用是松绑定，即使引用变量初始化时指向了一个变量，后期还可以修改其指向其他变量。
+	ref = 3.14159;
+	cout<<"ref ="<<ref<<endl;
+	if(&ref != nullptr)
+	{
+		delete &ref;
+		// &ref = nullptr;     //double &ref引用取地址后，相当于Sample* const ref指针，所以不能改变其指向
+	}
+
+#ifdef _ERROR_
+	ref = 5.55555;             //引用的堆内存变量已被delete掉了,继续访问内存被释放掉的变量，会造成软件崩溃
+	cout<<"ref ="<<ref<<endl;
+#endif
+	
+	Sample num(5);
 	num.display();
 	
 	int &i = num.getNum();    //将类的私有成员引用出来使用
 	i++;
 	num.display();
 	
-	cout<<"n="<<n<<" ,n*n="<<fun(n)<<endl;
-	
 	Sample s1(10),s2(4),s3(15);
 	s3 = add(s1,s2);
 	s3.display();
-	
 	Sample& r = *(new Sample(25));            //C++中可以将new出来的堆对象赋值给引用，即可以引用堆对象
 	r.display();
 	
--- C++/Language/template/add.cpp
+++ C++/Language/template/add.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,12 +10,26 @@ T add(const T& x,const T& y)
 	return x+y;
 }
 
+template <>
+string add(const string& x,const string& y)      //函数模板特例化时，可以更改其函数实现，使其行为与通用模板不一致。
+{
+	cout<<"Specialized template for string type"<<endl;
+	return x + y;
+}
+
 int main()
 {
 	int i=3,j=4;
 	double m=2.12,n=3.34;
+	string x="hello",y="world";
 	
-	cout <<i<<"+"<<j<<" ="<<add(i,j)<<endl;
-	cout <<m<<"+"<<n<<" ="<<add(m,n)<<endl;
+	cout<<i<<" + "<<j<<" ="<<add(i,j)<<endl;
+	cout<<m<<" + "<<n<<" ="<<add(m,n)<<endl;            //函数模板隐式具体化
+	
+	cout<<m<<" + "<<n<<" ="<<add<double>(m,n)<<endl;    //函数模板显示具体化
+	
+	string z = add(x,y);
+	cout<<x<<" + "<<y<<" ="<<z<<endl;                    //函数模板对string类型特例化
+	
 	return 0;
 }
