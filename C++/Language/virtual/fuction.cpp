9a78e6b46f7f1e85b27d4e30db59e70a7535ba31
18200399814 <2272203582@qq.com>
2021-05-19 23:50:10 +0800

    学习C++中子类通过::作用域访问符调用父类中的public函数，包括已经被子类覆盖的虚函数。

--- C++/Language/virtual/fuction.cpp
+++ C++/Language/virtual/fuction.cpp
@@ -28,6 +28,13 @@ public:
 	{
 		cout<<"class SubClass: foo "<<x<<endl;
 	}
+	
+	void bar()
+	{
+		foo(2.71828);      //调用子类覆盖的foo()虚函数，即子类会优先调用自己实现的函数
+		Base::foo(5);      //子类可以通过::作用域符访问到父类中被覆盖了的foo()虚函数
+		cout<<"class SubClass: bar"<<endl;
+	}
 };
 
 int main()
@@ -42,5 +49,8 @@ int main()
 	(*p).fun(2);
 	p->foo(2);
 	p->foo(3.1415);
+	
+	b.Base::foo(1.414);   //子类对象可以通过::作用域符访问父类中任意public函数，包括被覆盖了的虚函数
 	b.foo(3.1415);
+	b.bar();
 }
