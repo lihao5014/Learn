a158276434ad06a1a4df67478d0629c8afbb6550
18200399814 <2272203582@qq.com>
2025-09-14 10:46:39 +0800

    学习使用CMake构建VS项目

---
+++ CMake/hellovscmake/hellovscmake.cpp
@@ -0,0 +1,15 @@
+﻿// hellovscmake.cpp: 定义应用程序的入口点。
+//
+
+#include "hellovscmake.h"
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake!" << endl;
+
+	getchar();
+	return 0;
+}
